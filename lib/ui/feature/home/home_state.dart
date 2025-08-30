cb56fc6b9a1ee6ee5f9d174e44c7301c2cbd271b
P/Am <anangpambudi101@gmail.com>
2025-08-30 16:46:40 +0700

    fix : update freezed

--- lib/ui/feature/home/home_state.dart
+++ lib/ui/feature/home/home_state.dart
@@ -3,14 +3,17 @@ import '../../../domain/models/user/user.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'home_state.freezed.dart';
+
 part 'home_state.g.dart';
 
 @freezed
-class HomeState with _$HomeState{
-@JsonSerializable(fieldRename: FieldRename.snake)
+abstract class HomeState with _$HomeState {
+  @JsonSerializable(fieldRename: FieldRename.snake)
   factory HomeState({
-   required User? user,
-   required List<User>? users,
-}) = _HomeState;
- factory HomeState.fromJson(Map<String, dynamic> json) => _$HomeStateFromJson(json);
+    required User? user,
+    required List<User>? users,
+  }) = _HomeState;
+
+  factory HomeState.fromJson(Map<String, dynamic> json) =>
+      _$HomeStateFromJson(json);
 }
